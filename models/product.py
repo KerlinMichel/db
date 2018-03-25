3707362ba61f764867d465e1f2b8582889e9bd12
KerlinMichel <michelkerlin@gmail.com>
2018-03-25 14:53:22 -0400

    fixed typo

--- models/product.py
+++ models/product.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, Datetime, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
